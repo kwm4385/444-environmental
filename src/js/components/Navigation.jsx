500ac9a262c33e2f081da23ffdb882bc592a7882
Kevin Moses <kmoses28@gmail.com>
2016-04-17 21:25:48 -0400

    Install jquery and foundation via npm. Revert index.html and move nav to component

--- src/js/components/Navigation.jsx
+++ src/js/components/Navigation.jsx
@@ -10,7 +10,17 @@ export default React.createClass({
 
   render() {
     return (
-      <p>Hello, world!</p>
+      <nav className="top-bar app-menu-bar" id="nav-menu">
+        <div className="top-bar-left">
+          <ul className="vertical medium-horizontal menu">
+            <li className="app-menu-item"><a href="#">Home</a></li>
+            <li className="app-menu-item"><a href="#">Tips</a></li>
+            <li className="app-menu-item"><a href="#">Leaderboards</a></li>
+            <li className="app-menu-item"><a href="#">Events</a></li>
+          </ul>
+        </div>
+
+      </nav>
     );
   }
 });
