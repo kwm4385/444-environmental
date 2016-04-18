43290d85531ce4f0aca62bc63239de3488f2021b
Kevin Moses <kmoses28@gmail.com>
2016-04-17 21:37:57 -0400

    Concat css into bundle

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,10 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
 var config = require('../config.js').sass;
+var concatCss = require('gulp-concat-css');
 
 gulp.task('styles', function() {
   gulp.src(config.src)
     .pipe(sass(config.settings))
+    .pipe(concatCss("bundle.css"))
     .pipe(gulp.dest(config.dest))
     .pipe(connect.reload());
 });
