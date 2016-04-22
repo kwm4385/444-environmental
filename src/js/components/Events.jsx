565471907d586ecfb9e276695799c2ecd1faa7d0
Michael Timbrook <timbrook480@gmail.com>
2016-04-21 22:57:51 -0400

    Events list

--- src/js/components/Events.jsx
+++ src/js/components/Events.jsx
@@ -1,23 +1,55 @@
 import React  from 'react'
 import Section  from './Section.jsx'
+import { Link } from 'react-router'
 import Navigation from './Navigation.jsx'
 
 export default React.createClass({
   getInitialState() {
-    console.log("what");
-    return {};
+    return {
+      selected: 0
+    };
   },
 
   componentDidMount() {
-    console.log("didMount");
+  },
+
+  eventsFor(list) {
+    
+    let events = Array(16)
+      .join(".") // Because Array(16) is [undefined x 16] and join works on that
+      .split(".")// but map cant because ^ that is not the same as [undefined, ...]
+      .map( (n, i) => {
+      return (<tr key={i}>
+        <td>Recyle Drive</td>
+      </tr>)
+    });
+    return events;
   },
 
   render() {
     return (
       <div>
         <Navigation active="events" /> 
+        <div>
+          <div className="row">
+            <div className="small-4 large-4 columns e-nav">
+              <Link to='/events/popular' className="item"> Popular </Link>
+            </div>
+            <div className="small-4 large-4 columns e-nav">
+              <Link to='/events/MyList' className="item"> My List </Link>
+            </div>
+            <div className="small-4 large-4 columns e-nav">
+              <Link to='/events/all' className="item"> All </Link>
+            </div>
+          </div>
+        </div>
         <Section>
           <h1>Events</h1>
+          <table>
+            <tbody>
+              {this.eventsFor(this.state.selected)}
+            </tbody>
+          </table>
         </Section>
       </div>
     );
