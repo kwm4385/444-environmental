11e51fecaa3fc8e9b18f164cf7f1c4f9405c9da6
Kevin Moses <kmoses28@gmail.com>
2016-04-23 23:51:33 -0400

    Create wizard framework

--- src/js/components/EventWizard.jsx
+++ src/js/components/EventWizard.jsx
@@ -1,30 +1,83 @@
 import React  from 'react'
 import { Row, Column, Button, Colors, Sizes } from 'react-foundation'
+import _  from 'underscore'
 
 export default React.createClass({
   getInitialState() {
     return {
-      step: 0
+      step: 0,
+      title: "",
+      description: "",
+      location: "",
+      date: null,
+      points: 0
     };
   },
 
   componentWillMount() {
     this.steps = [
-      this._stepOne()
+      this._stepOne(),
+      <p>2</p>
     ];
+  },
+
+  _forward() {
+    if (this.state.step < _.size(this.steps) - 1) {
+      this.setState({
+        step: this.state.step + 1
+      });
+    }
+  },
+
+  _back() {
+    if (this.state.step > 0) {
+      this.setState({
+        step: this.state.step - 1
+      });
+    }
+  },
+
+  _fieldChange(name, e) {
+    let change = {};
+    change[name] = e.target.value;
+    this.setState(change);
   },
 
   _stepOne() {
     return (
-      <Row>
-        <Column small={12} style={{textAlign:'center', marginTop:'20%'}}>
-          <p>Create your own sustainability event in a few easy steps!</p>
-        </Column>
-      </Row>
+      <div>
+        <Row>
+          <Column small={12} style={{textAlign:'center', marginTop:'20%'}}>
+            <p>Create your own sustainability event in a few easy steps!</p>
+          </Column>
+        </Row>
+        <Row>
+          <Column small={12} style={{marginTop:'10px'}}>
+            <label>Title:</label>
+            <input type="text" name="title" onChange={this._fieldChange.bind(this, 'title')}></input>
+          </Column>
+        </Row>
+      </div>
     );
   },
 
   render() {
-    return this.steps[this.state.step];
+    return (
+      <div>
+        {this.steps[this.state.step]}
+        <Row style={{marginTop:'10px'}}>
+          <Column small={6} className="button-col" style={{padding:'0px 10px'}}>
+            <Button color={Colors.SECONDARY} disabled={this.state.step <= 0} onClick={this._back}>
+              Previous
+            </Button>
+          </Column>
+          <Column small={6} className="button-col" style={{padding:'0px 10px'}}>
+            <Button onClick={this._forward}>
+              {this.state.step < _.size(this.steps) - 1 ? 'Next' : 'Finish'}
+            </Button>
+          </Column>
+        </Row>
+      </div>
+    );
   }
 });
