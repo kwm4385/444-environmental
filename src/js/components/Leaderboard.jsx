e323a9406989cc82e87e3e38307b5c7a3a62accb
Kevin Moses <kmoses28@gmail.com>
2016-04-23 22:01:11 -0400

    Leaderboard

--- src/js/components/Leaderboard.jsx
+++ src/js/components/Leaderboard.jsx
@@ -1,21 +1,45 @@
 import React  from 'react'
-import Navigation  from './Navigation.jsx'
+import { Row, Column } from 'react-foundation'
+import { Link } from 'react-router'
+import _  from 'underscore'
+import UserStore  from '../stores/UserStore.js'
+import List  from './List.jsx'
 import Section  from './Section.jsx'
 
 export default React.createClass({
+  _onChange() {
+    this.setState(EventStore.getAll());
+  },
+
   getInitialState() {
-    return {};
+    return UserStore.getAll();
   },
 
   componentDidMount() {
+    UserStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount() {
+    UserStore.removeChangeListener(this._onChange);
+  },
+
+  _renderListItem(u) {
+    return (
+      <Row>
+        <Column small={7}>
+          {u.name}
+        </Column>
+        <Column small={5}>
+          {u.points} Points
+        </Column>
+      </Row>
+    );
   },
 
   render() {
     return (
-      <div>
-        <Section>
-          <h1>Leaderboard</h1>
-        </Section>
+      <div style={{height:'calc(100% - 25px)'}}>
+        <List items={_.sortBy(this.state.users, 'points').reverse()} itemRenderer={this._renderListItem}></List>
       </div>
     );
   }
--- src/js/components/List.jsx
+++ src/js/components/List.jsx
@@ -9,7 +9,12 @@ export default React.createClass({
 
   renderItems(items, itemRenderer, linkPrefix) {
     return _.map(items, function(e, i) {
-      return <Link key={i} to={linkPrefix + '/' + e.id}><li>{itemRenderer(e)}</li></Link>;
+      console.log(linkPrefix);
+      if (linkPrefix) {
+        return <Link key={i} to={linkPrefix + '/' + e.id}><li>{itemRenderer(e)}</li></Link>;
+      } else {
+        return <li key={i}>{itemRenderer(e)}</li>;
+      }
     });
   },
 
