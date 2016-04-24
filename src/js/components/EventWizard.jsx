c1d98f1d225171f875ca8db55828c93821ad60b7
Kevin Moses <kmoses28@gmail.com>
2016-04-24 00:13:55 -0400

    More wizardry

--- src/js/components/EventWizard.jsx
+++ src/js/components/EventWizard.jsx
@@ -16,8 +16,8 @@ export default React.createClass({
 
   componentWillMount() {
     this.steps = [
-      this._stepOne(),
-      <p>2</p>
+      this._stepOne,
+      this._stepTwo
     ];
   },
 
@@ -47,36 +47,67 @@ export default React.createClass({
     return (
       <div>
         <Row>
-          <Column small={12} style={{textAlign:'center', marginTop:'20%'}}>
+          <Column small={12} style={{textAlign:'center', marginTop:'10%'}}>
             <p>Create your own sustainability event in a few easy steps!</p>
           </Column>
         </Row>
         <Row>
           <Column small={12} style={{marginTop:'10px'}}>
             <label>Title:</label>
-            <input type="text" name="title" onChange={this._fieldChange.bind(this, 'title')}></input>
+            <input type="text" name="title" value={this.state.title} onChange={this._fieldChange.bind(this, 'title')}></input>
           </Column>
         </Row>
+        {this._controls(this.state.title.length > 0)}
       </div>
+    );
+  },
+
+  _stepTwo() {
+    return (
+      <div>
+        <Row>
+          <Column small={12} style={{textAlign:'center', marginTop:'10%'}}>
+            <h1>{this.state.title}</h1>
+          </Column>
+        </Row>
+        <Row>
+          <Column small={12} style={{marginTop:'10px'}}>
+            <label>Description:</label>
+            <input
+              type="textarea"
+              name="description"
+              value={this.state.description}
+              onChange={this._fieldChange.bind(this, 'description')}
+              style={{width:'100%', height:'8em'}}></input>
+          </Column>
+        </Row>
+        {this._controls(true)}
+      </div>
+    );
+  },
+
+  _controls(valid) {
+    return (
+      <Row style={{marginTop:'10px'}}>
+        <Column small={6} className="button-col" style={{padding:'0px 10px'}}>
+          <Button color={Colors.SECONDARY} disabled={this.state.step <= 0} onClick={this._back}>
+            Previous
+          </Button>
+        </Column>
+        <Column small={6} className="button-col" style={{padding:'0px 10px'}}>
+          <Button onClick={this._forward} disabled={!valid}>
+            {this.state.step < _.size(this.steps) ? 'Next' : 'Submit'}
+          </Button>
+        </Column>
+      </Row>
     );
   },
 
   render() {
     return (
-      <div>
-        {this.steps[this.state.step]}
-        <Row style={{marginTop:'10px'}}>
-          <Column small={6} className="button-col" style={{padding:'0px 10px'}}>
-            <Button color={Colors.SECONDARY} disabled={this.state.step <= 0} onClick={this._back}>
-              Previous
-            </Button>
-          </Column>
-          <Column small={6} className="button-col" style={{padding:'0px 10px'}}>
-            <Button onClick={this._forward}>
-              {this.state.step < _.size(this.steps) - 1 ? 'Next' : 'Finish'}
-            </Button>
-          </Column>
-        </Row>
+      <div style={{paddingTop:'10px'}}>
+        <h1 style={{textAlign:'center'}}>Create Event</h1>
+        {this.steps[this.state.step]()}
       </div>
     );
   }
