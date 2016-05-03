5e542b488ebe3e1998710ef9b7dda63d7181215c
Kevin Moses <kmoses28@gmail.com>
2016-05-03 16:45:34 -0400

    Event checkin w/ qr scanning

--- src/js/components/EventCheckin.jsx
+++ src/js/components/EventCheckin.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router'
 import Constants  from '../Constants'
 import Dispatcher  from '../Dispatcher'
 import EventStore  from '../stores/EventStore.js'
+import QRReader  from './QRReader.jsx'
 import Section  from './Section.jsx'
 
 export default React.createClass({
@@ -15,7 +16,8 @@ export default React.createClass({
 
   getInitialState() {
     return {
-      event: EventStore.getEvent(this.props.params.eventId)
+      event: EventStore.getEvent(this.props.params.eventId),
+      scanned: false
     };
   },
 
@@ -27,26 +29,55 @@ export default React.createClass({
     EventStore.removeChangeListener(this._onChange);
   },
 
-  render() {
+  onQRRead(data) {
+    console.log(data);
+    this.setState({
+      scanned: true
+    });
+  },
+
+  renderContent() {
     let e = this.state.event;
-    return (
-      <div>
-        <Row>
-          <Column style={{textAlign:'center'}} small={12}>
-            <h1>{e.title}</h1>
-          </Column>
-        </Row>
-        <Section className="roomy padded">
+    if (!this.state.scanned) {
+      return (
+        <div>
           <Row className="event-details">
             <Column small={12} style={{textAlign:'center'}}>
               <h2>Scan the event QR code</h2>
             </Column>
           </Row>
-            <Row className="event-details">
-              <Column small={12} style={{textAlign:'center'}}>
-                SCANNER
-              </Column>
-            </Row>
+          <Row className="event-details">
+            <Column small={12} style={{textAlign:'center'}}>
+              <QRReader onRead={this.onQRRead}/>
+            </Column>
+          </Row>
+        </div>
+      );
+    } else {
+      return (
+        <Row className="event-details">
+          <Column small={12} style={{textAlign:'center'}}>
+            <h2>Submission Recieved!</h2>
+            <p>You've recieved {e.points} points!</p>
+            <Link to="/">
+              <Button size={Sizes.SMALL}>Okay</Button>
+            </Link>
+          </Column>
+        </Row>
+      );
+    }
+  },
+
+  render() {
+    return (
+      <div>
+        <Row>
+          <Column style={{textAlign:'center'}} small={12}>
+            <h1>{this.state.event.title}</h1>
+          </Column>
+        </Row>
+        <Section className="roomy padded">
+          {this.renderContent()}
         </Section>
       </div>
     );
